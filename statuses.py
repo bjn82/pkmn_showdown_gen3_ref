1b4a6c1173f86fe789597198c55d640329ae27ae
Brennan Donnell <bjn82@wildcats.unh.edu>
2019-04-16 14:06:06 -0400

    Unfinished
    Statuses are not done, but I wanted to make a branch of it to come back to later.

---
+++ statuses.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 13:18:47 2019
+
+@author: brennandonnell
+"""
+#%%  IMPORTS
+
+#import pickle
+
+#%%
+"""
+Basic setup about this:
+{
+Status: status name,
+Description:, text
+}
+"""
+
+burned = {
+        'Status': '',
+        'Description': ''
+        }
+
+confused = {
+        'Status': '',
+        'Description': ''
+        }
+
+frozen = {
+        'Status': '',
+        'Description': ''
+        }
+
+#infatuated 
+
+#%%
